291d8b0e970c89dc4dafaa01af719bebc6c13382
W. Guilherme <wguilhermecriativo@gmail.com>
2020-01-21 01:04:05 -0300

    [Update] Search response in Json + filter $in key params

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -1,14 +1,23 @@
 const Dev = require('../models/Dev')
 const parseStringAsArray = require('../utils/parseStringAsArray')
+//buscar todos devs num radio de 10kkm
+//filtrar por tecnologias
 
 module.exports = {
     async index(request, response) {
         const { latitude, longitude, techs } = request.query
         const techsArray = parseStringAsArray(techs);
 
-        console.log(techsArray)
-        //buscar todos devs num radio de 10kkm
-        //filtrar por tecnologias
-        return response.json({ devs: [] })
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+        })
+
+
+
+        
+  
+        return response.json({ devs })
     }
 }
