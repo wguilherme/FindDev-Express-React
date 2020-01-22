cd67b3235aa2ea86f19bef77a9ba82c20d1e661d
W. Guilherme <wguilhermecriativo@gmail.com>
2020-01-22 00:22:16 -0300

    [Update] Separate component Header

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from './Header';
 
+// Component
+  //função que retorna conteúdo em html (ou css / js de interface)
+// Estado
+// Propriedade
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Header />
   );
 }
 
